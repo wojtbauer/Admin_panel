05c3f59a43fe029d545dfc95e4ed7e82c4fc7e12
wojtbauer <wojciech.brk1@gmail.com>
2018-09-04 09:46:20 +0200

    creation of superuser

--- SampleApp/admin.py
+++ SampleApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from SampleApp.models import SampleModel_1, SampleModel_2
+
+
 # Register your models here.
+
+admin.site.register(SampleModel_1)
+admin.site.register(SampleModel_2)
+
