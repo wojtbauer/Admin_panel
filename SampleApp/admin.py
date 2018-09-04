8604e69501a7bd936d793cba7f294b55a287f142
wojtbauer <wojciech.brk1@gmail.com>
2018-09-04 12:11:49 +0200

    Added objects possibilities

--- SampleApp/admin.py
+++ SampleApp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from SampleApp.models import SampleModel_1, SampleModel_2
+from SampleApp.models import SampleModel_1, SampleModel_2, Entry_1, Entry_2
 
 
 # Register your models here.
 
 admin.site.register(SampleModel_1)
 admin.site.register(SampleModel_2)
+admin.site.register(Entry_1)
+admin.site.register(Entry_2)
 
--- SampleApp/models.py
+++ SampleApp/models.py
@@ -14,3 +14,24 @@ class SampleModel_2(models.Model):
     
     def __str__(self):
         return self.text
+
+
+class Entry_1(models.Model):
+    Sample_obj1 = models.ForeignKey('SampleModel_1', on_delete=models.CASCADE)
+    text = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = 'entries_1'
+        
+    def __str__(self):
+        return self.text[:50] + "..."
+        
+class Entry_2(models.Model):
+    Sample_obj2 = models.ForeignKey('SampleModel_2', on_delete=models.CASCADE)
+    text = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = 'entries_2'
+        
+    def __str__(self):
+        return self.text[:50] + "..."
