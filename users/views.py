5157ba3c0b1243b6e9119e66090bc030f970c1cd
wojtbauer <wojciech.brk1@gmail.com>
2018-09-05 17:31:54 +0200

    Accounts registration feature

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate 
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    if request.method != "POST":
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username=new_user.username,
+                password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('SampleApp:index'))
+            
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
